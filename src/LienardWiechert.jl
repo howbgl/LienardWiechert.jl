9bbb331bb5505e7160dd9e263a4211595c4ea2c4
how09898 <wolfgang.hogger@gmail.com>
2026-02-10 16:44:23 +0100

    Added SpaceTimeCurve struct.

--- src/LienardWiechert.jl
+++ src/LienardWiechert.jl
@@ -1,5 +1,25 @@
 module LienardWiechert
 
-# Write your package code here.
+using ArgCheck
+using LinearAlgebra
+
+export SpaceTimeCurve
+
+
+"Discrete space-time curve with times t and coordinatex x,y,z."
+struct SpaceTimeCurve{T}
+    t::Vector{T}
+    x::Vector{T}
+    y::Vector{T}
+    z::Vector{T}
+    function SpaceTimeCurve{T}(t::Vector{T}, x::Vector{T}, y::Vector{T}, z::Vector{T}) where T
+        @argcheck length(t) == length(x) == length(y) == length(z) "All input vectors must have the same length."
+        new{T}(t, x, y, z)
+    end
+end
+function SpaceTimeCurve(t::Vector{<:Real}, x::Vector{<:Real}, y::Vector{<:Real}, z::Vector{<:Real}) 
+    args = promote(t, x, y, z)
+    SpaceTimeCurve{eltype(args[1])}(args...)
+end
 
 end
