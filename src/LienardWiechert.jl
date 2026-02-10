5ee9f9928cf031394bee65bc2c9906cc2dd7e1fc
how09898 <wolfgang.hogger@gmail.com>
2026-02-10 17:04:34 +0100

    Added minkowsi distance.

--- src/LienardWiechert.jl
+++ src/LienardWiechert.jl
@@ -6,7 +6,7 @@ using LinearAlgebra
 export SpaceTimeCurve
 
 
-"Discrete space-time curve with times t and coordinatex x,y,z."
+"Discrete space-time curve with times t and space coordinates x,y, and z."
 struct SpaceTimeCurve{T}
     t::Vector{T}
     x::Vector{T}
@@ -20,6 +20,29 @@ end
 function SpaceTimeCurve(t::Vector{<:Real}, x::Vector{<:Real}, y::Vector{<:Real}, z::Vector{<:Real}) 
     args = promote(t, x, y, z)
     SpaceTimeCurve{eltype(args[1])}(args...)
+end
+
+"Calculate the Minkowski distance squared between two spacetime points, (+---) signature."
+function minkowski_distance_squared(x1,x2)
+    dx = x1 .- x2
+    return dx[1]^2 - sum(dx[2:end].^2)
+end
+
+function find_equal_time(time::Real, c::SpaceTimeCurve)
+    for i in eachindex(c.t)
+        # check if time is in the interval [c.t[i], c.t[i+1]]
+        if i < length(c.t) && c.t[i] <= time <= c.t[i+1]
+            return i
+        end
+    end
+    throw(ArgumentError("Time $time is out of bounds of the curve's time range."))
+end
+
+function retarded_time(spacetime_point, c::SpaceTimeCurve)
+    for i in eachindex(c.t)
+        r = [c.t[i], c.x[i], c.y[i], c.z[i]]
+        # implement rest ...
+    end
 end
 
 end
