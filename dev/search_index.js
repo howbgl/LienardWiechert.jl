47a66084d2f542e389832d2a8d7be437d473c760
Documenter.jl <documenter@juliadocs.github.io>
2026-02-10 15:46:25 +0000

    build based on 9bbb331

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#LienardWiechert","page":"Home","title":"LienardWiechert","text":"Documentation for LienardWiechert.\n\n","category":"section"}]
+[{"location":"#LienardWiechert","page":"Home","title":"LienardWiechert","text":"Documentation for LienardWiechert.\n\n","category":"section"},{"location":"#LienardWiechert.SpaceTimeCurve","page":"Home","title":"LienardWiechert.SpaceTimeCurve","text":"Discrete space-time curve with times t and coordinatex x,y,z.\n\n\n\n\n\n","category":"type"}]
 }
